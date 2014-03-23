1b30b12419ef2f4ac4473b46809ad8fe0ce1e612
郑一 <Gintama.Zheng@gmail.com>
2014-03-23 14:44:21 +0800

    Entiry Header

---
+++ Core/YYEntities.h
@@ -0,0 +1,18 @@
+//
+//  YYEntities.h
+//  YYEngineDemo
+//
+//  Created by 郑一 on 14-3-23.
+//  Copyright (c) 2014年 郑一. All rights reserved.
+//
+
+#ifndef YYEngineDemo_YYEntities_h
+#define YYEngineDemo_YYEntities_h
+
+struct YYPoint
+{
+    float x;
+    float y;
+};
+
+#endif
