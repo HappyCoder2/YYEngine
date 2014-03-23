31bbd4c4dd09a8545963d3c2f120d59ac6f9d66b
郑一 <Gintama.Zheng@gmail.com>
2014-03-23 13:57:18 +0800

    Render Header File

---
+++ Core/YYRender.h
@@ -0,0 +1,16 @@
+//
+//  YYRender.h
+//  YYEngineDemo
+//
+//  Created by 郑一 on 14-3-23.
+//  Copyright (c) 2014年 郑一. All rights reserved.
+//
+
+#ifndef YYEngineDemo_YYRender_h
+#define YYEngineDemo_YYRender_h
+
+void init(void);
+
+void display(void);
+
+#endif
